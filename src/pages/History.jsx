edd1f51122d9b9d462f914c1cc20dec92b4cd479
JONATHAN ALVAREZ valeK <valekjonathan@gmail.com>
2025-12-09 22:34:42 +0000

    File changes

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -154,7 +154,7 @@ export default function History() {
         </div>
       </header>
 
-      <main className="pt-20 pb-32 px-4">
+      <main className="pt-16 pb-32 px-4">
         <Tabs defaultValue="alerts" className="w-full">
           <TabsList className="w-full bg-gray-900 border border-gray-800 mb-2">
             <TabsTrigger value="alerts" className="flex-1 data-[state=active]:bg-purple-600">
