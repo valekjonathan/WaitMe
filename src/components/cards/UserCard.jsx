998f72b6b0961c4c200b829e6b09b8ade7e70b17
JONATHAN ALVAREZ valeK <valekjonathan@gmail.com>
2025-12-10 20:57:56 +0000

    Mejor precio para vehículos

--- src/components/cards/UserCard.jsx
+++ src/components/cards/UserCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapPin, Clock, Navigation, MessageCircle, Phone } from 'lucide-react';
+import { MapPin, Clock, Navigation } from 'lucide-react';
 import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
 import { Button } from '@/components/ui/button';
