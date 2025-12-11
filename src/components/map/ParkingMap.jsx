63eb86032c0ef1453d6e558e6261ef026e1018ba
JONATHAN ALVAREZ valeK <valekjonathan@gmail.com>
2025-12-11 19:20:47 +0000

    Ajustes Menú Inferior

--- src/components/map/ParkingMap.jsx
+++ src/components/map/ParkingMap.jsx
@@ -141,7 +141,7 @@ function FlyToLocation({ position }) {
 
   useEffect(() => {
     if (position) {
-      map.setView(position, 16, { animate: true, duration: 1 });
+      map.flyTo(position, 16);
     }
   }, [position, map]);
 
