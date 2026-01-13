cc052022f7e4d0927251871cd25285bfd75aa62e
JONATHAN ALVAREZ valeK <valekjonathan@gmail.com>
2026-01-13 23:25:07 +0000

    File changes

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -482,7 +482,7 @@ export default function Chat() {
         )}
         </div>
 
-      {/* Messages */}
+        {/* Messages */}
       <main className="flex-1 pt-96 pb-20 px-4 overflow-y-auto">
         <div className="mb-4 mt-2 p-4 bg-purple-600/20 border border-purple-500 rounded-lg">
           <p className="text-base text-purple-300 font-bold">Ultimos mensajes:</p>
