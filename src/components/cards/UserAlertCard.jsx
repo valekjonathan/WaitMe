c988129d0287c86af2c9058133cc42bf6ff2d422
JONATHAN ALVAREZ valeK <valekjonathan@gmail.com>
2026-01-13 13:54:40 +0000

    Manual code change

--- src/components/cards/UserAlertCard.jsx
+++ src/components/cards/UserAlertCard.jsx
@@ -64,7 +64,7 @@ export default function UserAlertCard({
             }
             </Button>
 
-            <Button className="bg-purple-600 text-white ml-4 px-4 py-2 text-sm font-semibold rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow flex-1 hover:bg-purple-700 h-9"
+            <Button className="bg-purple-600 text-white ml-6 px-4 py-2 text-sm font-semibold rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow flex-1 hover:bg-purple-700 h-9"
 
           onClick={() => onBuyAlert(alert)}
           disabled={isLoading}>
