e7b985a21d457249f44a0930de18a571210be41d
JONATHAN ALVAREZ valeK <valekjonathan@gmail.com>
2026-01-13 14:00:31 +0000

    File changes

--- src/components/cards/UserAlertCard.jsx
+++ src/components/cards/UserAlertCard.jsx
@@ -53,7 +53,7 @@ export default function UserAlertCard({
             <Button
             variant="outline"
             size="icon"
-            className={`border-gray-700 h-8 w-8 ${alert.allow_phone_calls ? 'hover:bg-gray-800' : 'opacity-40 cursor-not-allowed'}`}
+            className={`border-gray-700 h-8 w-[42px] ${alert.allow_phone_calls ? 'hover:bg-gray-800' : 'opacity-40 cursor-not-allowed'}`}
             onClick={() => alert.allow_phone_calls && onCall(alert)}
             disabled={!alert.allow_phone_calls}>
 
@@ -64,7 +64,7 @@ export default function UserAlertCard({
             }
             </Button>
 
-            <Button className="bg-purple-600 text-white ml-[34px] px-4 py-2 text-sm font-semibold rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow flex-1 hover:bg-purple-700 h-9"
+            <Button className="bg-purple-600 text-white ml-6 px-4 py-2 text-sm font-semibold rounded-md inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 shadow flex-1 hover:bg-purple-700 h-9"
 
           onClick={() => onBuyAlert(alert)}
           disabled={isLoading}>
