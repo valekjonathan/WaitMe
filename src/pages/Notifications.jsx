819e7ed4a50d597325f0ef32249264870d0ba93f
JONATHAN ALVAREZ valeK <valekjonathan@gmail.com>
2026-01-13 15:51:38 +0000

    File changes

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -11,7 +11,6 @@ import { format } from 'date-fns';
 import { es } from 'date-fns/locale';
 import BottomNav from '@/components/BottomNav';
 import UserCard from '@/components/cards/UserCard';
-import { MapPin } from 'lucide-react';
 
 export default function Notifications() {
   const [user, setUser] = useState(null);
