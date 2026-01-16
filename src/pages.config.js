2c4b616a94b5d56ae561eb22b1a62fd6cb379b85
JONATHAN ALVAREZ valeK <valekjonathan@gmail.com>
2026-01-16 13:52:45 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,24 +1,24 @@
 import Chat from './pages/Chat';
 import Chats from './pages/Chats';
-import Home from './pages/Home';
+import History from './pages/History';
 import Navigate from './pages/Navigate';
 import NotificationSettings from './pages/NotificationSettings';
 import Notifications from './pages/Notifications';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
-import History from './pages/History';
+import Home from './pages/Home';
 
 
 export const PAGES = {
     "Chat": Chat,
     "Chats": Chats,
-    "Home": Home,
+    "History": History,
     "Navigate": Navigate,
     "NotificationSettings": NotificationSettings,
     "Notifications": Notifications,
     "Profile": Profile,
     "Settings": Settings,
-    "History": History,
+    "Home": Home,
 }
 
 export const pagesConfig = {
