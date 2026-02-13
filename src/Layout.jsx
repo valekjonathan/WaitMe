5eafa587ae146d68ead1bb6b921753893eb227d1
valekjonathan <valekjonathan@gmail.com>
2026-02-13 22:02:42 +0100

    VACIO TODO DE NUeVO

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export default function Layout({ children }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Helmet>
+      {children}
+    </>
+  );
 }
