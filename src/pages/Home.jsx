6b93502164e2153c49e19f03e1a05cb5b37fda62
valekjonathan <valekjonathan@gmail.com>
2026-02-16 15:32:17 +0100

    cxsac

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -585,8 +585,7 @@ export default function Home() {
               className="fixed inset-0 top-[60px] flex flex-col"
               style={{ overflow: 'hidden', height: 'calc(100dvh - 60px)', paddingBottom: 'calc(env(safe-area-inset-bottom) + 88px)' }}
             >
-              <div className="relative px-3 pt-[14px] pb-2 flex-none"
-              style={{ height: "calc(40% + 30px)" }}>
+              <div className="relative px-3 pt-[14px] pb-2 flex-none" style={{ height: "calc(40% + 30px)" }}>
                 <ParkingMap
                   useCenterPin={true}
                   userLocation={userLocation}
