9e80ba67ce217852db275e8045ab03138cfd925e
valekjonathan <valekjonathan@gmail.com>
2026-02-16 20:31:23 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -617,7 +617,7 @@ export default function Home() {
                 </div>
               </div>
 
-              <div className="px-4 pt-0 flex-1 min-h-0 flex items-stretch">
+              <div className="px-4 pt-0 mt-[2px] flex-1 min-h-0 flex items-stretch">
                 <div className="w-full h-full">
                   <CreateAlertCard
                     address={address}
