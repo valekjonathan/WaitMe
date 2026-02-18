cb852cb7bd872bab8e679c76bda59d5320975c9b
valekjonathan <valekjonathan@gmail.com>
2026-02-18 11:52:46 +0100

    iphone

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -12,22 +12,24 @@ import NotificationSettings from './pages/NotificationSettings';
 
 export default function Layout() {
   return (
-    <Routes>
+    <div className="flex flex-col flex-1 w-full">
+      <div className="flex flex-col flex-1 w-full max-w-md mx-auto">
+        <Routes>
+          {/* HOME DIRECTO SIN REDIRECCIONES */}
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/Home" element={<Home />} />
 
-      {/* HOME DIRECTO SIN REDIRECCIONES */}
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/Home" element={<Home />} />
-
-      <Route path="/chats" element={<Chats />} />
-      <Route path="/chat/:id" element={<Chat />} />
-      <Route path="/notifications" element={<Notifications />} />
-      <Route path="/notification-settings" element={<NotificationSettings />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/settings" element={<Settings />} />
-      <Route path="/history" element={<History />} />
-      <Route path="/navigate" element={<NavigatePage />} />
-
-    </Routes>
+          <Route path="/chats" element={<Chats />} />
+          <Route path="/chat/:id" element={<Chat />} />
+          <Route path="/notifications" element={<Notifications />} />
+          <Route path="/notification-settings" element={<NotificationSettings />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/navigate" element={<NavigatePage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
