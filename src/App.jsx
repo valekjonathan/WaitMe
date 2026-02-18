0dba9a6802e9f396e5afe1d80d75246651b6db5b
valekjonathan <valekjonathan@gmail.com>
2026-02-18 11:37:42 +0100

    igual que iphone

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import DemoFlowManager from '@/components/DemoFlowManager';
 
 export default function App() {
   return (
-    <>
+    <div className="min-h-[100dvh] bg-black pb-[env(safe-area-inset-bottom)]">
       {/* Arranca el flujo demo y mantiene datos sincronizados (sin UI extra) */}
       <DemoFlowManager />
       <Layout />
-    </>
+    </div>
   );
 }
